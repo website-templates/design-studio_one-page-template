c7a54837f078fd3e2295f56f908a95cd960e4283
Maxim Orlov <mail@orlovmax.com>
2014-10-23 01:32:56 +0300

    structure rebuild with front-end scaffold

---
+++ Gruntfile.js
@@ -0,0 +1,364 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+      //Assemble *.js files
+        concat: {
+          main: {
+            files: [{
+              expand: true,
+              src: ['dev/js/*.js', '!dev/js/assembled.js'],
+              dest: 'dev/js/assembled.js'
+            }]
+          },
+          head: {
+            files: [{
+              expand: true,
+              src: ['dev/js/head/*.js', '!dev/js/head/head.js'],
+              dest: 'dev/js/head/head.js'
+            }]
+          }
+        },
+
+      //Uglify assembled *.js file    
+        uglify: {
+          options: {
+            mangle: false
+          },
+          vendor: {
+            files: [{
+                expand: true,
+                cwd: 'dev/js/vendor',
+                src: '**/*.js',
+                dest: 'build/js/vendor',
+                ext: '.min.js'
+            }]
+          },
+          main: {
+              files: {
+                  'build/js/assembled.min.js': 'dev/js/assembled.js'
+              }
+          },
+          head: {
+            files: {
+                'build/js/head/head.min.js': 'dev/js/head/head.js'
+            }
+          }
+        },
+
+      //Compile *.scss files  
+        sass: {
+          main: { 
+            options: {
+              style: 'expanded',
+              sourcemap: 'none'
+            },
+            files: [{
+              expand: true,
+              cwd: 'dev/styles',
+              src: ['*.{sass,scss}'],
+              dest: 'dev/css',
+              ext: '.css'
+            }]
+          }
+        },
+
+      //Compile *.less files
+        less: {
+          main: {
+            files: [{
+              expand: true,
+              cwd: 'dev/styles',
+              src: ['*.less'],
+              dest: 'dev/css',
+              ext: '.css'
+            }]
+          }       
+        },  
+
+      //Combine media queries in result *.css files   
+        cmq: {
+          options: {
+            log: false
+          },
+          main: {
+            files: {
+              'dev/css': ['dev/css/*.css']
+            }
+          }
+        },
+
+      //Autoprefixer  
+        autoprefixer: {
+          options: {
+            browsers: ['last 2 versions', 'ie 8', 'ie 9'] 
+            //By default >1%, last 2 versions, Firefox ESR, Opera 12.1;
+          },           
+          main: {
+            files:[{
+              expand: true,
+              flatten: true,
+              src: 'dev/css/*.css',
+              dest: 'dev/css/'
+            }]
+          }
+        },
+
+      //Minify and organize *.css files  
+        csso: {
+          options: {
+            keepSpecialComments: '*'
+          },
+          main: {
+            files:[{
+              expand: true,
+              cwd: 'dev/css/',
+              src: ['*.css', '!*.min.css'],
+              dest: 'build/css/',
+              ext: '.min.css'
+            }]  
+          }
+        },
+
+      //Compile *.jade files  
+        jade: {
+          main: {
+            options: {
+                client: false,
+                pretty: true
+            },
+            files: [ {
+              cwd: "dev/markup",
+              src: "*.jade",
+              dest: "dev/html/",
+              expand: true,
+              ext: ".html"
+            } ]
+          }
+        },
+
+      //Compile *.haml files  
+        haml: {
+          main: {
+            files: [ {
+              cwd: "dev/markup",
+              src: "*.haml",
+              dest: "dev/html/",
+              expand: true,
+              ext: ".html"
+            } ]
+          }
+        },
+
+      //Minify *.html files 
+        htmlmin: {  
+            main: {     
+              options: {             
+                collapseWhitespace: true,
+                minifyJS: true,
+                minifyCSS: true
+              },
+              files: [ {
+                cwd: "dev/html",
+                src: "*.html",
+                dest: "build/",
+                expand: true,
+                ext: ".html"
+              } ]
+            }
+        },
+
+      //Minify image files   
+        imagemin: {
+          main: { 
+            options: { 
+              optimizationLevel: 7
+            },              
+            files: [{
+              expand: true,      
+              cwd: 'dev/img',            
+              src: ['**/*.{png,jpg,gif}'], 
+              dest: 'build/img/'            
+            }]
+          }
+        },
+
+      //Copy some folders or files (ex. *.php) from dev to build
+        copy: {
+          php: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/php/',
+              src: '**', 
+              dest: 'build/php/'
+            }]
+          },
+          fonts: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/fonts/',
+              src: ['**/*.{eot,svg,ttf,woff}'], 
+              dest: 'build/fonts/'
+            }]
+          },
+          js: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/js/',
+              src: [
+                '**/assembled.js', 
+                '**/vendor.js', 
+                '**/head.js'],
+              dest: 'build/js/'
+            }]
+          },  
+          livejs: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/devtools/',
+              src: '**/live.js',
+              dest: 'build/js/'
+            }]
+          },        
+          css: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/css',
+              src: ['**/*.css'],
+              dest: 'build/css/'
+            }]
+          },        
+          html: {
+            files: [{
+              expand: true, 
+              cwd: 'dev/html',
+              src: ['**/*.html'],
+              dest: 'build/'
+            }]
+          }
+        },   
+
+      //Assemble bower components in right order 
+        bower_concat: {
+          main: {
+            dest: 'dev/js/vendor/vendor.js'
+          }
+        },
+
+      //Copy bower components to the custom folder   
+        // bowercopy: {
+        //   options: {
+        //     clean: true,
+        //     ignore: ['modernizr']
+        //   },          
+        //   jquery: {
+        //     options: {
+        //         destPrefix: 'dev/js/vendor'
+        //     },
+        //     files: {
+        //       'jquery': 'jquery/dist/jquery.js'
+        //     },
+        //   }
+        // },
+
+      //Delete .gitkeep files. If you don't use Bower - just run `grunt clean`  
+        clean: {
+          gitkeep: ['dev/**/.gitkeep', 'build/**/.gitkeep'],
+          less: 'dev/**/*.less',
+          sass: 'dev/**/*.scss',
+          haml: 'dev/**/*.haml',
+          jade: 'dev/**/*.jade',
+          debug: ['build/js/**/*.js', 
+                  '!build/js/**/*.min.js', 
+                  'build/css/**/*.css',
+                  '!build/css/**/*.min.css'],
+          bower: 'bower_components'   
+        },
+
+      //Delete some dev code and references from files        
+        preprocess : {
+          html : {
+            src : [ 'dev/html/*.html' ],
+            options: {
+              inline : true
+            }
+          },
+          css : {
+            src : [ 'dev/css/*.css' ],
+            options: {
+              inline : true
+            }
+          },
+          js : {
+            src : [ 'dev/js/*.js' ],
+            options: {
+              inline : true
+            }
+          }
+        },
+
+      //Watch for changes    
+        watch: {
+          all: {
+            files: ['dev/html/**/*.html', 
+                    'dev/styles/**/*.{scss,less}', 
+                    'dev/css/*.css',
+                    'dev/js/**/*.js', 
+                    'dev/img/**/*.{png,jpg,gif}',
+                    'dev/markup/**/*.{haml,jade}',
+                    'dev/php/**/*.php',
+                    'dev/fonts/**/*.{eot,svg,ttf,woff}'],
+            tasks: ['default'],
+            options: {
+              spawn: false,
+            },
+          },
+        }
+
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-combine-media-queries');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-haml2html');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-newer');
+
+    grunt.loadNpmTasks('grunt-bower-concat');
+    // grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.loadNpmTasks('grunt-preprocess');
+
+    grunt.registerTask('default', ['newer:concat', 
+                                   'newer:sass', 
+                                   'newer:less', 
+                                   'newer:jade',
+                                   'newer:haml',
+                                   'newer:imagemin',
+                                   'newer:copy',
+                                   'watch'
+    ]);
+
+    grunt.registerTask('bower-dev', ['bower_concat',
+                                     // 'bowercopy',
+                                     'clean:gitkeep',
+                                     'clean:bower'
+    ]);
+
+    grunt.registerTask('build', ['preprocess',
+                                 'cmq',
+                                 'autoprefixer',
+                                 'uglify',
+                                 'csso',
+                                 'htmlmin',                                     
+                                 'clean:debug'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,22 +1,41 @@
-Free design studio one page template
-======
+#Free design studio one page template
+
 Flat and responsive website template, designed by cssauthor.com
 ###### Powered by html5, css3, Scss and a little bit of js and jQuery.
 ###### Compatible Browsers: IE8+, Firefox, Chrome, Opera, Safari, Yandex.browser
-Demo
-------
+
+## Quick start
+Actually this example is ready to use (build version), all dependencies already installed. But for further development you should use grunt and bower, see more details about installation grunt [here](https://github.com/orlovmax/front-end-scaffold#how-to-start) and about bower [here](https://github.com/orlovmax/front-end-scaffold#bower)
+
+To compile this project, please run the following:
+
+* `npm install` - install grunt packages
+* `bower install` - install dependencies
+    - `grunt bower-dev` - compile dependencies and remove bower_components folder
+* `grunt` - default task, compile project files
+
+To create build version, please run build task:
+* `grunt build`
+
+### Structure
+This project based on [frontend-scaffold](https://github.com/orlovmax/front-end-scaffold):
+
+`/dev/` folder - contains source code.
+
+`/build/` folder - contain ready-to-use build version.
+
+##Demo
 You can see live demo here: [http://orlovmax.com/freebies/design-studio_one-page-template](http://orlovmax.com/freebies/design-studio_one-page-template "design studio  template live demo")
 
 Please note
 ---
 In reset.css most of elements `box-sizing` sets to `border-box`. 
 
-Screenshots
----
+##Screenshots
 **You can see more screenshots [here](screenshots/)**
 
 ![Mockup demo](screenshots/pic.jpg)
-
----
-
 Product mockup created with [http://frame.lab25.co.uk/](http://frame.lab25.co.uk/)
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
